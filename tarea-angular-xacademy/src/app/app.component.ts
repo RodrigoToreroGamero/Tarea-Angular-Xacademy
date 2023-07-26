345b853c730e6023bf19041fc3bc5dbe3868aea0
Rodrigo Torero <rodrigo.torero.gamero@gmail.com>
2023-07-26 11:49:24 -0500

    lista y boton creados

--- tarea-angular-xacademy/src/app/app.component.ts
+++ tarea-angular-xacademy/src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+type Product = {
+  name: string;
+  price: number;
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tarea-angular-xacademy';
+
+  listIsVisible: boolean = true;
+  textOnHiddenList: string = "Mostrar lista";
+  textOnVisibleList: string = "Ocultar lista";
+  textListState: string = "";
+
+  ShowOrHideList(): void {
+    this.listIsVisible = !this.listIsVisible;
+    if(this.listIsVisible) {
+      this.textListState = this.textOnVisibleList;
+    }
+    else {
+      this.textListState = this.textOnHiddenList;
+    }
+  }
+
+  ngOnInit(): void {
+    this.ShowOrHideList();
+  }
+
+  products: Product[] = [
+    { name: "Lapicero", price: 6.5 },
+    { name: "Cuaderno", price: 10 },
+    { name: "Regla", price: 3 },
+    { name: "Pegamento", price: 1.5 },
+    { name: "Tijeras", price: 4 },
+    { name: "Cartuchera", price: 13.5 },
+  ];
 }
